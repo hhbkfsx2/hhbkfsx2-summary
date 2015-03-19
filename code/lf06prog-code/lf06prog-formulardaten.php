b39c31e382c1f871cb9e145a4e86e782fd216c07
Kilian <kilian.engelhardt@gmail.com>
2015-03-19 21:49:23 +0100

    kleines Update

---
+++ code/lf06prog-code/lf06prog-formulardaten.php
@@ -0,0 +1,11 @@
+<?php
+ #Variablen setzen
+ $textvar = $_POST["txt_variablenname"];
+ $rbvar = $_POST["rb_variablenname"];
+ $cbvar = $_POST["cb_variablenname"];
+ 
+ #Zuweisung einer GET-Variablen:
+ #$get_var = $_GET["variablenname"];
+ 
+ echo "Inhalt der Variable" $textvar:" $textvar";
+?>
